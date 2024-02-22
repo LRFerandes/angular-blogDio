3bd9a3956dbd7845ae34066ae564d517a30432f9
Lauro Nunes <lauroruan08@gmail.com>
2024-02-22 11:34:29 -0300

    card grande

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { TituloComponent } from './titulo/titulo.component';
+import { CardGrandeComponent } from './card-grande/card-grande.component';
+import { CardPequenoComponent } from './card-pequeno/card-pequeno.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    TituloComponent
+    TituloComponent,
+    CardGrandeComponent,
+    CardPequenoComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/card-grande/card-grande.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit,  Input } from '@angular/core';
+
+@Component({
+  selector: 'app-card-grande',
+  templateUrl: './card-grande.component.html',
+  styleUrls: ['./card-grande.component.css']
+})
+export class CardGrandeComponent implements OnInit {
+
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
